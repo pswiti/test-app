3d9ad96b91423e0a267402c8b01bc91a78febaff
Switi <45126465+pswiti@users.noreply.github.com>
2025-06-18 12:34:42 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,20 +2,39 @@ provider "azurerm" {
   features {}
   subscription_id = "sub-id"
 }
-
 resource "azurerm_resource_group" "rg" {
-  name     = "staticweb-rg"
-  location = "East US"
+  name     = var.resource_group_name
+  location = var.location
 }
 
-resource "azurerm_storage_account" "static" {
-  name                     = "staticwebdemo123"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  static_website {
-    index_document = "index.html"
-    error_404_document = "404.html"
+resource "azurerm_container_app_environment" "env" {
+  name                = var.environment_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_container_app" "app" {
+  name                         = var.container_app_name
+  container_app_environment_id = azurerm_container_app_environment.env.id
+  resource_group_name          = azurerm_resource_group.rg.name
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = "myapp"
+      image  = var.container_image
+      cpu    = 0.25
+      memory = "0.5Gi"
+
+      ports {
+        port     = 80
+        protocol = "TCP"
+      }
+    }
+  }
+
+  ingress {
+    external_enabled = true
+    target_port      = 80
   }
 }
